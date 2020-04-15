6cca091cc4a178ca47e8cad4ff39165f48904cc5
Gaëtan <gaetan.herve@lilo.org>
2020-04-15 08:28:56 +0200

    init

---
+++ MODULE2_TP1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MODULE2_TP1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
