ace1af7c2de7791113554fdb94b1cf49f4539305
Gaëtan <gaetan.herve@lilo.org>
2020-04-21 14:47:19 +0200

    Project init + database creation

---
+++ MODULE6_TP1/Data/MODULE6_TP1Context.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace MODULE6_TP1.Data
+{
+    public class MODULE6_TP1Context : DbContext
+    {
+        // You can add custom code to this file. Changes will not be overwritten.
+        // 
+        // If you want Entity Framework to drop and regenerate your database
+        // automatically whenever you change your model schema, please use data migrations.
+        // For more information refer to the documentation:
+        // http://msdn.microsoft.com/en-us/data/jj591621.aspx
+    
+        public MODULE6_TP1Context() : base("name=MODULE6_TP1Context")
+        {
+        }
+
+        public System.Data.Entity.DbSet<MODULE6_TP1_BO.Samourai> Samourais { get; set; }
+
+        public System.Data.Entity.DbSet<MODULE6_TP1_BO.Arme> Armes { get; set; }
+    }
+}
---
+++ MODULE6_TP1_BO/Samourai.cs
@@ -0,0 +1,10 @@
+﻿namespace MODULE6_TP1_BO
+{
+    public class Samourai
+    {
+        public int Id { get; set; }
+        public int Force { get; set; }
+        public string Nom { get; set; }
+        public virtual Arme Arme { get; set; }
+    }
+}
